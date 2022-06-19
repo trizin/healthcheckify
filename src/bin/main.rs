4bd3244c7cca73d8ed9f7f807d9f9591e728503c
trizin <25263018+trizin@users.noreply.github.com>
2022-06-19 20:13:48 +0000

    Read .env

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    env,
     fs::read_to_string,
     sync::{Arc, Mutex},
 };
@@ -11,11 +12,19 @@ use healthchecker::{
 
 #[tokio::main]
 async fn main() {
-    let addr = "0.0.0.0:8080";
+    dotenv::dotenv().ok();
+
+    let addr = env::var("BIND_ADDR").unwrap_or_else(|_| String::from("127.0.0.1:8080"));
+    let thread_count = env::var("THREAD_COUNT")
+        .unwrap_or_else(|_| String::from("5"))
+        .parse::<usize>()
+        .unwrap();
+
     let config_file = read_to_string("./config.json").unwrap();
     let health_checker = HealthChecker::new(config_file);
-    let pool = ThreadPool::new(5).unwrap();
-    let lstnr = listener::listen(addr).expect("Cannot listen address");
+    let pool = ThreadPool::new(thread_count).unwrap();
+
+    let lstnr = listener::listen(&addr).expect("Cannot listen address");
 
     let hc = Arc::new(Mutex::new(health_checker));
 
