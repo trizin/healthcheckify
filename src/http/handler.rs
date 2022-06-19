7906cd7177bec1e8122bf98e93d4a5b7e2392c57
trizin <25263018+trizin@users.noreply.github.com>
2022-06-19 21:41:30 +0000

    Better error handling

--- src/http/handler.rs
+++ src/http/handler.rs
@@ -1,34 +1,43 @@
 use std::io::{Read, Write};
 
+use regex::Regex;
+use std::error::Error;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
-
-use regex::Regex;
 
 use crate::healthcheck::health_checker::HealthChecker;
 use crate::healthcheck::node::model::NodeStatus;
 
-pub fn handle_connection(mut stream: TcpStream, health_checker: Arc<Mutex<HealthChecker>>) {
+pub fn handle_connection(
+    mut stream: TcpStream,
+    health_checker: Arc<Mutex<HealthChecker>>,
+) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0; 1024];
-    Read::read(&mut stream, &mut buffer).unwrap();
+    Read::read(&mut stream, &mut buffer)?;
 
     let text = String::from_utf8_lossy(&buffer);
-    let re = Regex::new("GET /(.*) HTTP").unwrap();
-    let path = re.captures(&text).unwrap();
-    let path = &path[1].replace('"', "");
+    let re = Regex::new("GET /(.*) HTTP")?;
+    let path = re.captures(&text);
+    if let Some(_path) = path {
+        if _path.len() > 1 {
+            let path = &_path[1].replace('"', "");
+            let stat = health_checker.lock().unwrap().check_by_id(path);
 
-    let stat = health_checker.lock().unwrap().check_by_id(path);
+            let response = match stat {
+                Ok(stat) if stat == NodeStatus::Down => get_response("error", 500),
+                Ok(stat) if stat == NodeStatus::Healthy => get_response("ok", 200),
+                Ok(stat) if stat == NodeStatus::Processing => get_response("ok", 200),
+                Ok(_) => get_response("not found", 404),
+                Err(_) => get_response("not found", 404),
+            };
 
-    let response = match stat {
-        Ok(stat) if stat == NodeStatus::Down => get_response("error", 500),
-        Ok(stat) if stat == NodeStatus::Healthy => get_response("ok", 200),
-        Ok(stat) if stat == NodeStatus::Processing => get_response("ok", 200),
-        Ok(_) => get_response("not found", 404),
-        Err(_) => get_response("not found", 404),
-    };
+            Write::write(&mut stream, &response)?;
+            stream.flush()?;
 
-    Write::write(&mut stream, &response).unwrap();
-    stream.flush().unwrap();
+            return Ok(());
+        }
+    }
+    Err("Couldn't find the path".into())
 }
 
 fn get_response(message: &str, response_code: u32) -> Vec<u8> {
